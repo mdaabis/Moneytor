7e4d7957b1ff875355360a027a0c533c0cfb4712
mdaabis <42125154+mdaabis@users.noreply.github.com>
2019-12-30 23:45:52 +0000

    Buttons working, preparation for navigation drawer.

--- app/src/main/java/com/example/moneytor/MainActivity.java
+++ app/src/main/java/com/example/moneytor/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.moneytor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.register);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
--- app/src/main/java/com/example/moneytor/RegisterPage.java
+++ app/src/main/java/com/example/moneytor/RegisterPage.java
@@ -2,13 +2,25 @@ package com.example.moneytor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterPage extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        btn = (Button) findViewById(R.id.login_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (RegisterPage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
