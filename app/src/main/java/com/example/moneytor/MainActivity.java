7cafbcd9e758cd1843fbf0fd32318081e8249257
mdaabis <42125154+mdaabis@users.noreply.github.com>
2020-01-05 03:16:08 +0000

    Basic front-end skeleton complete. About to begin login and authentication.

--- app/src/main/java/com/example/moneytor/MainActivity.java
+++ app/src/main/java/com/example/moneytor/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.moneytor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button reg_btn, login_btn;
-
+    private Button reg_btn, login_btn;
+    private EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         reg_btn = (Button)findViewById(R.id.register);
+        login_btn = (Button) findViewById(R.id.login_button);
+        email = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
         reg_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegisterPage.class);
-                startActivity(intent);
+                changeActivity(MainActivity.this, RegisterPage.class);
             }
         });
 
-        login_btn = (Button) findViewById(R.id.login_button);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomePage.class);
-                startActivity(intent);
+                changeActivity(MainActivity.this, HomePage.class);
             }
         });
 
 
+    }
+
+    public void changeActivity(Activity Current, Class Target){
+        Intent intent = new Intent(Current, Target);
+        startActivity(intent);
     }
 
 
